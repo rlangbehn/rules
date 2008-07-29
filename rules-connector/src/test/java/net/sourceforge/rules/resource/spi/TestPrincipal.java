7acce1ed3a859dda087d56eb0692232ab7eb6cee
rlangbehn <rlangbehn@75c46563-164f-0410-9d01-e95978e0f367>
2008-07-29 17:33:19 +0000

    initial revision

---
+++ rules-connector/src/test/java/net/sourceforge/rules/resource/spi/TestPrincipal.java
@@ -0,0 +1,83 @@
+/*****************************************************************************
+ * $Id$
+ *
+ * Copyright 2008, The Rules Framework Development Team, and individual
+ * contributors as indicated by the @authors tag. See the copyright.txt
+ * in the distribution for a full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ****************************************************************************/
+package net.sourceforge.rules.resource.spi;
+
+import java.security.Principal;
+
+/**
+ * TODO
+ * 
+ * @version $Revision$ $Date$
+ * @author <a href="mailto:rlangbehn@users.sourceforge.net">Rainer Langbehn</a>
+ */
+public class TestPrincipal implements Principal
+{
+	/**
+	 * TODO
+	 */
+	private String name;
+
+	/**
+	 * TODO
+	 * 
+	 * @param name
+	 */
+	public TestPrincipal(String name) {
+		this.name = name;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.security.Principal#getName()
+	 */
+	public String getName() {
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof TestPrincipal))
+			return false;
+		TestPrincipal other = (TestPrincipal) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+}
