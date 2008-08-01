b6d36abe268c1c181fde32bcc680268a1e83ae64
rlangbehn <rlangbehn@75c46563-164f-0410-9d01-e95978e0f367>
2008-08-01 18:54:21 +0000

    initial revision

---
+++ trunk/rules-service/rules-service-impl/src/main/java/net/sourceforge/rules/service/Messages.java
@@ -0,0 +1,162 @@
+/*****************************************************************************
+ * $Id: Messages.java 166 2008-08-01 14:28:40Z rlangbehn $
+ *
+ * Copyright 2008, The Rules Framework Development Team, and individual
+ * contributors as indicated by the @authors tag. See the copyright.txt
+ * in the distribution for a full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ****************************************************************************/
+package net.sourceforge.rules.service;
+
+import java.text.MessageFormat;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+/**
+ * TODO
+ * 
+ * @version $Revision: 166 $ $Date: 2008-08-01 16:28:40 +0200 (Fr, 01 Aug 2008) $
+ * @author <a href="mailto:rlangbehn@users.sourceforge.net">Rainer Langbehn</a>
+ */
+public final class Messages
+{
+    // Constants -------------------------------------------------------------
+	
+	/**
+	 * TODO
+	 */
+	private static final String BUNDLE_NAME =
+		"net.sourceforge.rules.service.messages"; //$NON-NLS-1$
+
+	/**
+	 * TODO
+	 */
+	private static final ResourceBundle RESOURCE_BUNDLE =
+		ResourceBundle.getBundle(BUNDLE_NAME);
+
+	// Attributes ------------------------------------------------------------
+
+    // Static ----------------------------------------------------------------
+	
+	/**
+	 * TODO
+	 *
+	 * @param key
+	 * @return
+	 */
+	public static String getError(String key) {
+		String s = "net.sourceforge.rules.service.error." + key; //$NON-NLS-1$
+		return getString(s);
+	}
+
+	/**
+	 * TODO
+	 *
+	 * @param key
+	 * @param arguments
+	 * @return
+	 */
+	public static String getError(String key, Object... arguments) {
+		String s = "net.sourceforge.rules.service.error." + key; //$NON-NLS-1$
+		return getString(s, arguments);
+	}
+
+	/**
+	 * TODO
+	 *
+	 * @param key
+	 * @return
+	 */
+	public static String getInfo(String key) {
+		String s = "net.sourceforge.rules.service.info." + key; //$NON-NLS-1$
+		return getString(s);
+	}
+
+	/**
+	 * TODO
+	 *
+	 * @param key
+	 * @param arguments
+	 * @return
+	 */
+	public static String getInfo(String key, Object... arguments) {
+		String s = "net.sourceforge.rules.service.info." + key; //$NON-NLS-1$
+		return getString(s, arguments);
+	}
+
+	/**
+	 * @param key
+	 * @return
+	 */
+	public static String getString(String key) {
+		try {
+			return RESOURCE_BUNDLE.getString(key);
+		} catch (MissingResourceException e) {
+			return '!' + key + '!';
+		}
+	}
+
+	/**
+	 * TODO
+	 *
+	 * @param key
+	 * @param arguments
+	 * @return
+	 */
+	public static String getString(String key, Object... arguments) {
+		String theString = getString(key);
+		return MessageFormat.format(theString, arguments);
+	}
+	
+	/**
+	 * TODO
+	 *
+	 * @param key
+	 * @return
+	 */
+	public static String getWarning(String key) {
+		String s = "net.sourceforge.rules.service.warning." + key; //$NON-NLS-1$
+		return getString(s);
+	}
+
+	/**
+	 * TODO
+	 *
+	 * @param key
+	 * @param arguments
+	 * @return
+	 */
+	public static String getWarning(String key, Object... arguments) {
+		String s = "net.sourceforge.rules.service.warning." + key; //$NON-NLS-1$
+		return getString(s, arguments);
+	}
+
+    // Constructors ----------------------------------------------------------
+	
+	/**
+	 * Private default ctor to prevent instantiation.
+	 */
+	private Messages() {
+	}
+	
+    // Public ----------------------------------------------------------------
+
+    // Package protected -----------------------------------------------------
+
+    // Protected -------------------------------------------------------------
+
+    // Private ---------------------------------------------------------------
+
+    // Inner classes ---------------------------------------------------------
+}
