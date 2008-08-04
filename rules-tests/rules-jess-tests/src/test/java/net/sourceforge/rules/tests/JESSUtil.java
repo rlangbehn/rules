fb77c89707b2ec985fd00f0157937ec52794ef18
rlangbehn <rlangbehn@75c46563-164f-0410-9d01-e95978e0f367>
2008-08-04 18:47:35 +0000

    initial revision

--- rules-tests/rules-jess-tests/src/test/java/net/sourceforge/rules/tests/JESSUtil.java
+++ rules-tests/rules-jess-tests/src/test/java/net/sourceforge/rules/tests/JESSUtil.java
@@ -1,5 +1,5 @@
 /*****************************************************************************
- * $Id: DroolsUtil.java 240 2008-08-04 15:20:18Z rlangbehn $
+ * $Id$
  *
  * Copyright 2008, The Rules Framework Development Team, and individual
  * contributors as indicated by the @authors tag. See the copyright.txt
@@ -32,7 +32,7 @@ import javax.rules.admin.RuleExecutionSet;
 /**
  * TODO
  * 
- * @version $Revision: 240 $ $Date: 2008-08-04 17:20:18 +0200 (Mo, 04 Aug 2008) $
+ * @version $Revision$ $Date$
  * @author <a href="mailto:rlangbehn@users.sourceforge.net">Rainer Langbehn</a>
  */
 public final class JESSUtil
