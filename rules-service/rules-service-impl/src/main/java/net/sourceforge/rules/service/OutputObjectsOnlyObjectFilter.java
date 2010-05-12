8356914c1c010aca32c735625af25959f87c7fbc
rlangbehn <rlangbehn@75c46563-164f-0410-9d01-e95978e0f367>
2010-05-12 11:44:22 +0000

    initial revision

--- rules-service/rules-service-impl/src/main/java/net/sourceforge/rules/service/OutputObjectsOnlyObjectFilter.java
+++ rules-service/rules-service-impl/src/main/java/net/sourceforge/rules/service/OutputObjectsOnlyObjectFilter.java
@@ -34,7 +34,7 @@ public class OutputObjectsOnlyObjectFilter implements ObjectFilter
 	// Constants -------------------------------------------------------------
 
 	/**
-	 * TODO
+	 * Default serial version UID.
 	 */
 	private static final long serialVersionUID = 1L;
 
