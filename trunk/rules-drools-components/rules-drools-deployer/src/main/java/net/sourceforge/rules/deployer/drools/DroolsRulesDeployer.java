4433dde452ea031f6ff63c67e17d5b1965878911
rlangbehn <rlangbehn@75c46563-164f-0410-9d01-e95978e0f367>
2010-06-02 22:57:36 +0000

    initial revision

---
+++ trunk/rules-drools-components/rules-drools-deployer/src/main/java/net/sourceforge/rules/deployer/drools/DroolsRulesDeployer.java
@@ -0,0 +1,66 @@
+/*****************************************************************************
+ * $Id$
+ *
+ * Copyright 2008, The Rules Framework Development Team, and individual
+ * contributors as indicated by the @authors tag. See the copyright.txt
+ * in the distribution for a full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ****************************************************************************/
+package net.sourceforge.rules.deployer.drools;
+
+import net.sourceforge.rules.deployer.AbstractRulesDeployer;
+import net.sourceforge.rules.deployer.RulesDeployerConfiguration;
+import net.sourceforge.rules.deployer.RulesDeployerException;
+
+/**
+ * TODO
+ *
+ * @plexus.component
+ *   role="net.sourceforge.rules.deployer.RulesDeployer"
+ *   role-hint="droolsd"
+ *
+ * @version $Revision$ $Date$
+ * @author <a href="mailto:rlangbehn@users.sourceforge.net">Rainer Langbehn</a>
+ */
+public class DroolsRulesDeployer extends AbstractRulesDeployer
+{
+	// Constants -------------------------------------------------------------
+
+	// Attributes ------------------------------------------------------------
+
+	// Static ----------------------------------------------------------------
+
+	// Constructors ----------------------------------------------------------
+
+	// AbstractRulesDeployer Overrides ---------------------------------------
+
+	/* (non-Javadoc)
+	 * @see net.sourceforge.rules.deployer.RulesDeployer#deploy(net.sourceforge.rules.deployer.RulesDeployerConfiguration)
+	 */
+	public void deploy(RulesDeployerConfiguration configuration)
+	throws RulesDeployerException {
+
+		if ((getLogger() != null) && getLogger().isInfoEnabled()) {
+			getLogger().info("deploy(" + configuration + ")");
+		}
+	}
+	
+	// Package protected -----------------------------------------------------
+
+	// Protected -------------------------------------------------------------
+
+	// Private ---------------------------------------------------------------
+
+	// Inner classes ---------------------------------------------------------
+}
